f540b29aa4840393b4b9d0f1d62a9dec7e1730dc
Raphael Collette <raphaelmcnyc@gmail.com>
2025-04-19 14:50:06 -0500

    fixed hashing when admin creates user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-from .models import User
+class CustomUserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
 
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
