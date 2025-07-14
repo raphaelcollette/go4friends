2b4bf687401728509d37430a0e9b5e5d68515650
Raphael Collette <raphaelmcnyc@gmail.com>
2025-07-14 02:28:26 -0500

    temporary debug mode

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'False'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['go4friends.onrender.com', 'go4friends.vercel.app','localhost', '127.0.0.1']
 
 FIELD_ENCRYPTION_KEY = os.getenv('ENCRYPTED_MODEL_FIELDS_KEY')
