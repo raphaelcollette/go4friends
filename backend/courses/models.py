cec3b0b87f35ca5af4ca7be4820c92952037e371
Raphael Collette <raphaelmcnyc@gmail.com>
2025-07-29 10:55:50 -0500

    course search backend fix again

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class ClassInfo(models.Model):
+    id = models.AutoField(primary_key=True)
     descr = models.TextField()
     full_name = models.TextField()
-    internet_id = models.TextField()
 
     class Meta:
         managed = False
         db_table = 'class_info'
-        unique_together = (('descr', 'full_name'),)
